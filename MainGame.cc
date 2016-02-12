872a189d59f7c143b3a9a5783bedc1b016b26868
Ondřej Budai <ondrej@budai.cz>
2016-02-12 18:52:35 +0100

    Vylepseno vykreslovani hraci desky v terminalu

--- MainGame.cc
+++ MainGame.cc
@@ -10,7 +10,7 @@
 namespace othello {
 
 
-    void MainGame::initPlayers(){
+    void MainGame::initPlayers() {
 
 
     }
@@ -18,35 +18,45 @@ namespace othello {
 
     //Ak uzivatel zada suradnice alebo staci prislusne okenko
     //vracia ukazatel na hraca ktory je na rade
-    Color MainGame::event(unsigned x, unsigned y, Color playedBy ){ // event funkce
+    Color MainGame::event(unsigned x, unsigned y, Color /*playedBy*/) { // event funkce
         //kontrola ci sa jedna o validny tah od uzivatela co je na rade(ako to kontrolovat?)
         //ak jedna ho nastavime
-        board_.setPiece(x,y, current_player_);
+        board_.setPiece(x, y, current_player_);
         //else ignorujeme (uzivatel moze klikat dobludu a nechceme ho na to upozronovat)
         // ak bol validny zisti ci dalsi hrac ma co hrat
         //ak ma, tak nastav current_player_ na dalsieho inak nemen (Bacha, kontrola ci maju obaja co
         //hrat!)
         //while (current_player == AI){
-            //AI.play();//play vyvolava tuto funckiu
-            //} 
+        //AI.play();//play vyvolava tuto funckiu
+        //}
         return current_player_;
 
     }
 
 
     //pomocna funkcia pre pracu bez gui 
-    void MainGame::printGameBoard(){//Zobrazi hraciu plochu na terminal
-        for (unsigned i = 0; i < board_.getSize(); i++){
-            for (unsigned j = 0; j < board_.getSize(); j++){
+    void MainGame::printGameBoard() {//Zobrazi hraciu plochu na terminal
+        using namespace std;
+        cout << "   ";
+        for (unsigned i = 0; i < board_.getSize(); i++) {
+            cout << i;
+            if (i < 10)
+                cout << " ";
+        }
+        cout << endl;
+        for (unsigned i = 0; i < board_.getSize(); i++) {
+            if (i < 10)
+                cout << " ";
+            cout << i << " ";
+            for (unsigned j = 0; j < board_.getSize(); j++) {
                 if (board_.isOccupied(i, j))
-                    std::cout<<board_.getColor(i,j)<<" ";
+                    cout << (board_.getColor(i, j) ? "\u25CB" : "\u25CD") << " ";
                 else
-                    std::cout<<"_ ";
+                    cout << "  ";
             }
-            std::cout<<std::endl;
+            cout << endl;
         }
     }
-
 
 
 }
--- main.cc
+++ main.cc
@@ -10,18 +10,18 @@ int main() {
     //vytvorime obrazovky
     //so vsetkym klikanim a pod
     //ak si vyberie novu hru a klikne start, tak sa zavola funkcia othello::MainGame(parametre)
-    othello::MainGame g(10);//toto potom tu nebude
+    othello::MainGame g(8);//toto potom tu nebude
     //samotne okno pri udalosti, vyvola funkcie, ktore su implementovane v gamelogic a v maingame
     //nasledujuca cast je pre terminal a tesotvanie
     othello::Color moves = othello::Color::BLACK;//alebo ten co zacina z menu sa vyberie
-    while (true){
+    while (true) {
         g.printGameBoard();
-        std::cout<<"Na tahu je "<< (moves==othello::Color::BLACK?"BLACK":"WHITE")<<std::endl;
-        std::cout<<"Zadaj suradnice: "<<std::flush;
+        std::cout << "Na tahu je " << (moves == othello::Color::BLACK ? "BLACK" : "WHITE") << std::endl;
+        std::cout << "Zadaj suradnice: " << std::flush;
         int x, y;
-        std::cin>>x>>y;
-        moves = g.event(x,y,moves);//but vrati tu istu farbu ak je druhy AI, alebo vrati opacnu  farbu
-                                 // ak ma tah...
-        
+        std::cin >> x >> y;
+        moves = g.event(x, y, moves);//but vrati tu istu farbu ak je druhy AI, alebo vrati opacnu  farbu
+        // ak ma tah...
+
     }
 }
