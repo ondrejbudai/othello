1b9b5f0cc90c67fcaedeec60145de4ec7534fef8
Ondřej Budai <ondrej@budai.cz>
2016-03-05 15:05:54 +0100

    More tabs even more fun

---
+++ MainWindow.cc
@@ -0,0 +1,59 @@
+#include "MainWindow.hh"
+#include "OthelloGui.hh"
+
+namespace othello {
+    MainWindow::MainWindow(QWidget* parent) : QTabWidget{parent}, ui{new Ui::MainWindow} {
+        ui->setupUi(this);
+
+        addGame();
+        connect(this, &MainWindow::tabBarClicked, this, &MainWindow::tabChangedSlot);
+        connect(this, &MainWindow::tabCloseRequested, this, &MainWindow::closeRequestSlot);
+    }
+
+    void MainWindow::tabChangedSlot(int index) {
+        // pokud menime na jiny nez posledni, event nas nezajima
+        if (index != count() - 1)
+            return;
+
+        // pridame hru
+        addGame();
+    }
+
+    void MainWindow::closeRequestSlot(int index) {
+        // pridavaci tab se nesmi zavrit
+        if (index == count() - 1)
+            return;
+
+        // odstran tab
+        removeTab(index);
+
+        // pokud zbyl pouze pridavaci tab, pridame novy
+        if (count() == 1)
+            addGame();
+
+        // pokud se pridavaci tab stane aktivnim, nastavime aktivnim tab predesly
+        if (index == count() - 1)
+            setCurrentIndex(index - 1);
+    }
+
+    void MainWindow::addGame() {
+        int tabs = count();
+
+        // smaz pridavaci tab, pokud existuje
+        if (tabs > 0)
+            removeTab(tabs - 1);
+
+        // nastavime jmeno hry
+        QString gameName("Game #");
+        gameName.append(QString::number(++games));
+
+        // pridame tab se hrou
+        addTab(new OthelloGui, gameName);
+
+        // pridame pridavaci tab
+        addTab(new QWidget, "+");
+
+        // smazeme zaviraci tlacitko pridavacimu tabu
+        tabBar()->setTabButton(count() - 1, QTabBar::RightSide, NULL);
+    }
+}
