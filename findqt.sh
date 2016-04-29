04040c3f829d909a16b4faafc90fd4988ec807fe
Ondřej Budai <ondrej@budai.cz>
2016-04-29 17:25:04 +0200

    Nyni se hackuje sestaveni pouze pro Merlina, normalni svet pouziva pkg-config

--- findqt.sh
+++ findqt.sh
@@ -1,35 +1,45 @@
 #!/usr/bin/env bash
-LOCATIONS=("/usr/lib64/qt5" "/usr/local/share/Qt-5.5.1/5.5/gcc_64")
-LIBS=("/usr/lib64/qt5" "/usr/local/share/Qt-5.5.1/5.5/gcc_64/lib")
-INCLUDES=("/usr/include/qt5" "/usr/local/share/Qt-5.5.1/5.5/gcc_64/include")
+LOCATIONS=("/usr/local/share/Qt-5.5.1/5.5/gcc_64/bin")
+LIBS=("/usr/local/share/Qt-5.5.1/5.5/gcc_64/lib")
+INCLUDES=("/usr/local/share/Qt-5.5.1/5.5/gcc_64/include")
 
-if [ ! $# = 1 ]; then
-    exit 0
+LOC=""
+LIB=""
+INC=""
+
+for loc in ${LOCATIONS[@]}; do
+    if [ -e "${loc}" ]; then
+        LOC="${loc}"
+    fi
+done
+
+if [ -z "$LOC" ]; then
+    LOC="`pkg-config --variable=bindir Qt5`"
 fi
 
-if [ $1 = "-f" ]; then
-    for loc in ${LOCATIONS[@]}; do
-        if [ -e "$loc" ]; then
-            echo "$loc"
-            exit 0
-        fi
-    done
+echo -n "QT_PATH=$LOC "
+
+for loc in ${LIBS[@]}; do
+    if [ -e "${loc}" ]; then
+        LIB="${loc}"
+    fi
+done
+
+if [ -z "$LIB" ]; then
+    LIB="`pkg-config --variable=libdir Qt5`"
 fi
 
-if [ $1 = "-l" ]; then
-    for loc in ${LIBS[@]}; do
-        if [ -e "$loc" ]; then
-            echo "$loc"
-            exit 0
-        fi
-    done
-fi
+echo -n "QT_LIB=$LIB "
 
-if [ $1 = "-i" ]; then
-    for loc in ${INCLUDES[@]}; do
-        if [ -e "$loc" ]; then
-            echo "$loc"
-            exit 0
-        fi
-    done
+for loc in ${INCLUDES[@]}; do
+    if [ -e "$loc" ]; then
+        INC="$loc"
+    fi
+done
+
+if [ -z "$INC" ]; then
+    INC="`pkg-config --variable=headerdir Qt5`"
 fi
+echo -n "QT_INC=$INC "
+
+echo
