d91d08fa07028fd9f5461b7a5cf176fd1f56d28a
Ondřej Budai <ondrej@budai.cz>
2016-05-04 20:40:55 +0200

    Iteratory pro GameBoard

--- src/logic/GameBoard.hh
+++ src/logic/GameBoard.hh
@@ -6,6 +6,8 @@
 #define OTHELLO_GAMEBOARD_HH
 
 #include<vector>
+#include<memory>
+#include<iostream>
 
 namespace othello {
 
@@ -41,6 +43,30 @@ namespace othello {
         Board board_;
         const unsigned size_;
 
+        template <typename T, typename C> class iterator_template {
+            public:
+                typedef iterator_template self_type;
+                typedef T value_type;
+                typedef T& reference;
+                typedef T* pointer;
+                typedef std::forward_iterator_tag iterator_category;
+                typedef int difference_type;
+
+                iterator_template(C* ptr, bool end = false) : ptr_{ptr}, size_{ptr->size()} {if(end) {y_ = size_;} else {y_ = 0;}}
+                self_type operator++() { self_type i = *this; x_++; if(x_ >= size_){ y_++; x_ = 0;} return i; }
+                self_type operator++(int) { x_++; if(x_ >= size_){ y_++; x_ = 0;} return *this; }
+                reference operator*() { return (*ptr_)[x_][y_]; }
+                pointer operator->() { return &(*ptr_)[x_][y_]; }
+                bool operator==(const self_type& b) { return ptr_ == b.ptr_ && x_ == b.x_ && y_ == b.y_; }
+                bool operator!=(const self_type& b) { return ptr_ != b.ptr_ || x_ != b.x_ || y_ != b.y_; }
+            private:
+                C* ptr_;
+                size_t x_ = 0, y_, size_;
+        };
+
+        using iterator = iterator_template<Field, Board>;
+        using const_iterator = iterator_template<const Field, const Board>;
+
     public:
         GameBoard(unsigned size);
 
@@ -63,6 +89,11 @@ namespace othello {
         Color GetColor(unsigned x, unsigned y) const;
 
         Color GetColor(const Coords& c) const { return GetColor(c.GetX(), c.GetY()); }
+
+        iterator begin(){return iterator{&board_};};
+        const_iterator begin() const {return const_iterator{&board_, true};};
+        iterator end(){return iterator{&board_};};
+        const_iterator end() const {return const_iterator{&board_, true};};
     };
 }
 
